c3dc93ad827b7981f3b59dcc7b6f7d11267abbfb
Nihilumbra03 <62447396+Nihilumbra03@users.noreply.github.com>
2021-07-14 18:35:05 +0300

    Higher power and bigger modulo support
    Now the program is guaranteed to work with powers up to 50000 and modulos up to 40000

--- Math utilities/Powers modulo N/main.cpp
+++ Math utilities/Powers modulo N/main.cpp
@@ -9,6 +9,14 @@ int power(int a, int b) {
   }
   return res;
 }
+int power_modulo(int a, int b, int m) {
+	int res = a % m;
+	for (int i=0; i<b-1; i++) {
+		res *= a;
+		res %= m;
+	}
+	return res;
+}
 void bubblesort(int * A, int n) {
  int swapped;
  for (int i=1; i<n; i++) {
@@ -18,9 +26,9 @@ void bubblesort(int * A, int n) {
     swap(A[j], A[j+1]);
 	swapped=1;
    }
- }
- if (!swapped)
+   if (!swapped)
 	 return;
+ }
 }
 int main() {
  cout<<"Please enter the natural power and the modulo"<<endl;
@@ -30,7 +38,7 @@ int main() {
   exit(0); 
  int * mas = new int[m];
  for (int i=0; i<m; i++) {
-   mas[i]=power(i, n)%m;
+   mas[i]=power_modulo(i, n, m);
  }
  bubblesort(mas, m);
  int x=-1;
@@ -40,6 +48,10 @@ int main() {
    cout<<mas[i]<<" ";
    x = mas[i];
  }
+ cout << endl;
+ /*int a, b, m;
+ cin >> a >> b >> m;
+ cout << power_modulo(a, b, m) << endl;*/
  system("pause");
  return 0;
 }
